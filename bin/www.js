dd5205e8f85dffaf6547242ec178e489589fafce
Sarah Levi <sarah75373@gmail.com>
2024-04-02 21:18:41 +0300

    read me

--- bin/www.js
+++ bin/www.js
@@ -12,7 +12,7 @@ import {
   initialCards,
 } from "../initialData/initalDataService.js";
 
-let port = normalizePort(process.env.PORT || "3030");
+let port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
 let server = http.createServer(app);
